2b427feeabcd9ab6cb7c635352a3bf8da9f5486b
shreyanshnpanwar <72238790+shreyanshnpanwar@users.noreply.github.com>
2020-10-02 10:53:57 +0530

    calculator using C

---
+++ calculate.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    char operator;
+    double first, second;
+    printf("Enter an operator (+, -, *,): ");
+    scanf("%c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &first, &second);
+
+    switch (operator) {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+        break;
+    case '-':
+        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+        break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+        break;
+        // operator doesn't match any case constant
+    default:
+        printf("Error! operator is not correct");
+    }
+
+    return 0;
+}
+# https://www.facebook.com/shreyansh.panwar.7/posts/1744222525725402
+# subscribed to code house
